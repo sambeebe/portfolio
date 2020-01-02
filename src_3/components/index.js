bd6bcc09248a4c8143384377a5b4844997ec98f1
sambeebe <samuelbeebe@gmail.com>
2020-01-02 01:52:51 -0500

    rfe

---
+++ src_3/components/index.js
@@ -0,0 +1,10 @@
+import BGImage from "./BGImage";
+import Container from "./Container";
+import App from "./Footer";
+import Hero from "./Hero";
+import Layout from "./Layout";
+import Navigation from "./Navigation";
+import ProjectItem from "./ProjectItem";
+import SEO from "./SEO";
+
+export { BGImage, Container, App, Hero, Layout, Navigation, ProjectItem, SEO };
