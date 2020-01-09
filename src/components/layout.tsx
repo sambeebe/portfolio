9a497ff78740823340dfbcc2dad5004663534cba
sambeebe <samuelbeebe@gmail.com>
2020-01-09 14:03:29 -0600

    update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,7 @@
-import React from "react"
+import * as React from "react";
+
+import * as ReactDOM from "react-dom";
+import { useMousePosition } from "./useMousePosition";
 import { Global } from "@emotion/core"
 import { Main, css, Styled } from "theme-ui"
 import useSiteMetadata from "../hooks/use-site-metadata"
@@ -6,6 +9,109 @@ import useNavigation from "../hooks/use-navigation"
 import Footer from "./footer"
 import Header from "./header"
 import SEO from "./seo"
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+//
+
+const style = {
+  height: 250,
+  width: 250, // we can control scene size by setting container dimensions
+  zIndex: -1
+};
+export class App extends React.Component<{}> {
+  componentDidMount() {
+    this.sceneSetup();
+    this.addCustomSceneObjects();
+    this.startAnimationLoop();
+    window.addEventListener("resize", this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowResize);
+    window.cancelAnimationFrame(this.requestID);
+    this.controls.dispose();
+  }
+
+  // Standard scene setup in Three.js. Check "Creating a scene" manual for more information
+  // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+  sceneSetup = () => {
+    // get container dimensions and use them for scene sizing
+    const width = this.el.clientWidth;
+    const height = this.el.clientHeight;
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75, // fov = field of view
+      width / height, // aspect ratio
+      0.1, // near plane
+      1000 // far plane
+    );
+    this.camera.position.z = 5; // is used here to set some distance from a cube that is located at z = 0
+    // OrbitControls allow a camera to orbit around the object
+    // https://threejs.org/docs/#examples/controls/OrbitControls
+    this.controls = new OrbitControls(this.camera, this.el);
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(width, height);
+    this.el.appendChild(this.renderer.domElement); // mount using React ref
+  };
+
+  // Here should come custom code.
+  // Code below is taken from Three.js BoxGeometry example
+  // https://threejs.org/docs/#api/en/geometries/BoxGeometry
+  addCustomSceneObjects = () => {
+    const geometry = new THREE.CubeGeometry(2);
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x156289,
+      emissive: 0x072534,
+      side: THREE.DoubleSide,
+      flatShading: true
+    });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    const lights = [];
+    lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+
+    lights[0].position.set(0, 200, 0);
+    lights[1].position.set(100, 200, 100);
+    lights[2].position.set(-100, -200, -100);
+
+    this.scene.add(lights[0]);
+    this.scene.add(lights[1]);
+    this.scene.add(lights[2]);
+  };
+
+  startAnimationLoop = () => {
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    this.renderer.render(this.scene, this.camera);
+
+    // The window.requestAnimationFrame() method tells the browser that you wish to perform
+    // an animation and requests that the browser call a specified function
+    // to update an animation before the next repaint
+    this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
+  };
+
+  handleWindowResize = () => {
+    const width = this.el.clientWidth;
+    const height = this.el.clientHeight;
+
+    this.renderer.setSize(width, height);
+    this.camera.aspect = width / height;
+
+    // Note that after making changes to most of camera properties you have to call
+    // .updateProjectionMatrix for the changes to take effect.
+    this.camera.updateProjectionMatrix();
+  };
+
+  render() {
+    return <div style={style} ref={ref => (this.el = ref)} />;
+  }
+}
 
 type LayoutProps = { children: React.ReactNode; className?: string }
 
@@ -15,6 +121,7 @@ const Layout = ({ children, className }: LayoutProps) => {
 
   return (
     <Styled.root data-testid="theme-root">
+
       <Global
         styles={css({
           "*": {
@@ -31,13 +138,17 @@ const Layout = ({ children, className }: LayoutProps) => {
             color: `background`,
           },
           a: {
-            transition: `all 0.1s ease-in-out`,
+            transition: `all 0.3s ease-in-out`,
           },
         })}
       />
       <SEO />
+      <App/>
       <Header meta={meta} nav={nav} />
+
       <Main className={className}>{children}</Main>
+
+      <Footer />
 
     </Styled.root>
   )
