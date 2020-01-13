8810c6ce64b1712649e3186139aabf65b2018efc
sambeebe <samuelbeebe@gmail.com>
2020-01-13 12:03:53 -0600

    update

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -60,7 +60,7 @@ const Projects = ({ projects }: Props) => {
     <Layout
       sx={{
         display: `grid`,
-        gridTemplateColumns: `repeat(auto-fit, minmax(200px, 1fr))`,
+        gridTemplateColumns: `repeat(auto-fit, minmax(150px, 1fr))`,
         width: `100%`,
       }}
     >
