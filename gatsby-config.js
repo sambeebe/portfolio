3448d194dbb5b9fbb2091aeb879f36d2c4a3a689
sambeebe <samuelbeebe@gmail.com>
2020-08-23 12:40:19 -0400

    update color

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,19 +27,10 @@ module.exports = {
         description: ``,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#b83280`,
+        theme_color: `#000000`,
         display: `standalone`,
         icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
+
         ],
       },
     },
