08851a2b8c782bb86859c0043626b39ff418e4b0
sambeebe <samuelbeebe@gmail.com>
2020-08-29 23:34:28 -0400

    updating favicon

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -30,8 +30,8 @@ const Project = ({ data: { project } }: ProjectProps) => {
     from: { opacity: 0, transform: `translate3d(0, -30px, 0)` },
     to: { opacity: 1, transform: `translate3d(0, 0, 0)` },
   })
-  const infoProps = useSpring({ config: config.slow, delay: 500, from: { opacity: 0 }, to: { opacity: 1 } })
-  const contentProps = useSpring({ config: config.slow, delay: 1000, from: { opacity: 0 }, to: { opacity: 1 } })
+  const infoProps = useSpring({ config: config.slow, delay: 500, from: { opacity: 1 }, to: { opacity: 1 } })
+  const contentProps = useSpring({ config: config.slow, delay: 1000, from: { opacity: 1 }, to: { opacity: 1 } })
 
   return (
     <Layout>
